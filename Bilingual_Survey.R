03b168530b2cc9367fa64643e5ee628b51d27b73
Reza Norouzian <rnorouzian@gmail.com>
2017-08-10 16:24:54 -0500

    Update Bilingual_Survey.R

--- Bilingual_Survey.R
+++ Bilingual_Survey.R
@@ -4,7 +4,7 @@ if(!random) set.seed(0) else set.seed(NULL)
   
 ResponseSequence = sample(x = 0:1, prob = c(1 - pYes, pYes), size = N, replace = TRUE)  # Generate a random sample of N responses
                                                                                         # (Yes = 1, No = 0)
-runProp = cumsum( ResponseSequence ) / 1:N   # Compute the running proportion of Yeses:
+runProp = cumsum(ResponseSequence) / 1:N   # Compute the running proportion of Yeses:
 
 original.par = par(no.readonly = TRUE)   ;  on.exit(par(original.par))    
 par(mgp = c(2, .5, 0), las = 1, tck = -.02, font.lab = 2, cex.lab = 1)
@@ -14,7 +14,7 @@ plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = 
     
 axis(2, at = seq(0, 1, len = 6), labels = paste0(seq(0, 100, len = 6), "%"))
     
-abline( h = pYes, lty = 2, col = 2 )
+abline(h = pYes, lty = 2, col = 2)
     
 text(mean(par("usr")[1:2]), pYes, "TRUE Proportion of \"YES\"", pos = 3, font = 2, col = 4)
     
@@ -22,7 +22,7 @@ ResponseLetters = paste( c("N","Y")[ResponseSequence[1:10] + 1], collapse ="" ) 
     
 displayString = paste0("Response Sequence = ", ResponseLetters, ". . .")
     
-text( N , c(1, .95), c(displayString, paste0("Proportion of Y in ", N, " responses = ", runProp[N])), adj = c(1, .5), col = "red4", font = 2, cex = .8)
+text(N, c(1, .95), c(displayString, paste0("Proportion of Y in ", N, " responses = ", runProp[N])), adj = c(1, .5), col = "red4", font = 2, cex = .8)
 }
 #Example of use:
 biling.survey(N = 1e3, pYes = .6)
