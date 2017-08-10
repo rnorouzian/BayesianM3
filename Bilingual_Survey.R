4708d84776609c7920738332570e1a32aab6b8a0
Reza Norouzian <rnorouzian@gmail.com>
2017-08-10 15:41:08 -0500

    Update Bilingual_Survey.R

--- Bilingual_Survey.R
+++ Bilingual_Survey.R
@@ -1,47 +1,29 @@
-biling.survey = function(N = 1e4, pYes = .5, random = TRUE, responses = FALSE){
+biling.survey = function(N = 40, pYes = .5, random = TRUE){
+                                               
+if(!random) set.seed(0) else set.seed(NULL) 
   
-  #  N     the total number of people to survey, denoted N.
-  #  pYes  TRUE probability of getting a Yes.  
-  if(!random) set.seed(0) else set.seed(NULL) 
+ResponseSequence = sample(x = 0:1, prob = c(1 - pYes, pYes), size = N, replace = TRUE)  # Generate a random sample of N responses
+                                                                                        # (Yes = 1, No = 0)
+runProp = cumsum( ResponseSequence ) / 1:N   # Compute the running proportion of Yeses:
   
-  # Generate a random sample of N responses (Yes = 1, No = 0):
-  ResponseSequence = sample(x = c(0, 1), prob = c(1 - pYes, pYes), size = N, replace = TRUE)
-  
-  # Compute the running proportion of Yeses:
-  r = cumsum( ResponseSequence ) # Cumulative sum of Yeses at end of n steps.
-  n = 1:N                        # listing the Number of Respondents at each step.
-  runProp = r / n                # Proportion of Yeses at end of n steps.
-  
-    original.par = par(no.readonly = TRUE)
-    on.exit(par(original.par))
-    par(mgp = c(2, .5, 0), las = 1, tck = -.02, font.lab = 2)
+par(mgp = c(2, .5, 0), las = 1, tck = -.02, font.lab = 2, cex.lab = 1)
     
-    plot.ts(runProp, ty = "o", ylim = c(0, 1), 
-            yaxt = "n", pch = 21, bg = 3, xlim = c(1, N),
-            xlab = "Number of Respondents", ylab = "Proportion of Yes",
-            main = "Bilingual Education Survey", cex = .9)
+plot.ts(runProp, ty = "o", ylim = c(0, 1), 
+        yaxt = "n", pch = 21, bg = 3,
+        xlab = "Number of Respondents", ylab = "Proportion of Yes",
+        main = "Bilingual Education Survey", cex = .9)
     
-    axis(1, at = 1)
-    axis(2, at = seq(0, 1, len = 6), labels = paste0(seq(0, 100, len = 6), "%"))
+axis(2, at = seq(0, 1, len = 6), labels = paste0(seq(0, 100, len = 6), "%"))
     
-    # Plot a dotted horizontal reference line:
-    abline( h = pYes, lty = 2, col = 2 )
+abline( h = pYes, lty = 2, col = 2 )
     
-    text(mean(par("usr")[1:2]), pYes, "TRUE Proportion of \"YES\"", pos = 3, font = 2, col = 4)
+text(mean(par("usr")[1:2]), pYes, "TRUE Proportion of \"YES\"", pos = 3, font = 2, col = 4)
     
-    # Display the first 10 response sequence:
-    ResponseLetters = paste( c("N","Y")[ResponseSequence[1:10] + 1], collapse ="" )
+ResponseLetters = paste( c("N","Y")[ResponseSequence[1:10] + 1], collapse ="" )    # Display the first 10 response sequence
     
-    displayString = paste0("Response Sequence = ", ResponseLetters, ". . .")
+displayString = paste0("Response Sequence = ", ResponseLetters, ". . .")
     
-    text( N , 1 , displayString , adj = c(1, .5), col = "red4", font = 2, cex = .8)
-    
-    # Display the relative frequency at the end of the sequence.
-    text( N , .95, paste0("Proportion of Y in ", N, " responses = ", runProp[N]) , adj = c(1, .5), 
-          col = "red4", font = 2, cex = .8 )
-    
-    if(responses)list(Responses = paste( c("N","Y")[ResponseSequence[1:N] + 1], collapse = "" ))
-  
+text( N , c(1, .95), c(displayString, paste0("Proportion of Y in ", N, " responses = ", runProp[N])), adj = c(1, .5), col = "red4", font = 2, cex = .8)
 }
 #Example of use:
 biling.survey(N = 1e3, pYes = .6)
