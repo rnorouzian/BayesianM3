83afef514d54fa20bc8cfea4c2725088f8df605a
Reza Norouzian <rnorouzian@gmail.com>
2017-08-12 12:24:37 -0500

    Update Bilingual_Survey.R

--- Bilingual_Survey.R
+++ Bilingual_Survey.R
@@ -7,7 +7,7 @@ biling.survey = function(N = 40, pBi = .5, ...){
   original.par = par(no.readonly = TRUE)   ;  on.exit(par(original.par))    
   par(mgp = c(2, .5, 0), las = 1, tck = -.02, font.lab = 2, cex.lab = 1, ...)
   
-  plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", ylab = "Proportion of Bilingual Ed.",
+  plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", ylab = "Proportion of (B)",
           main = "Bilingual Education Survey", cex = .9)
   
   axis(2, at = seq(0, 1L, len = 6), labels = paste0(seq(0, 1e2, len = 6), "%"))
