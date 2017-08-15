c68379fc7930e6df14ed8cf03751836a9af3e469
Reza Norouzian <rnorouzian@gmail.com>
2017-08-14 23:57:32 -0500

    Update Biling_Survey_Prior.R

--- Biling_Survey_Prior.R
+++ Biling_Survey_Prior.R
@@ -1,5 +1,5 @@
 C = c(15.56689246, 7.051444)
-B = curve(dbeta(x, C[1], C[2]), n = 1e4, axes = FALSE, lwd = 2, yaxs = "i", xpd = TRUE, 
+curve(dbeta(x, C[1], C[2]), n = 1e4, axes = FALSE, lwd = 2, yaxs = "i", xpd = TRUE, 
           xlab = "Proportion of preference for (B)", ylab = NA, font.lab = 2)
 axis(1, at = axTicks(1), labels = paste0(axTicks(1)*1e2, "%") )
 axis(1, at = axTicks(1)[4:5], labels = paste0(axTicks(1)[4:5]*1e2, "%"), font = 2)
