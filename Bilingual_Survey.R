d7f88a89cab1140d7c1b9bf4eb9a75ab40ad561c
Reza Norouzian <rnorouzian@gmail.com>
2017-08-16 16:28:45 -0500

    Update Bilingual_Survey.R

--- Bilingual_Survey.R
+++ Bilingual_Survey.R
@@ -4,17 +4,13 @@ biling.survey = function(N = 40, pBi = .5, ...){
                                                                                         # (B = 1, M = 0)
   runProp = cumsum(ResponseSequence) / 1:N   # Compute the running proportion of B:
    
-  par(mgp = c(2, .5, 0), las = 1, tck = -.02, font.lab = 2, cex.lab = 1, ...)  
-  plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", ylab = "Proportion of (B)",
-          main = "Bilingual Education Survey", cex = .9)
+  par(las = 1, tck = -.02, font.lab = 2, cex.lab = 1, ...)  
+  plot.ts(runProp, ty = "o", ylim = c(0, 1), yaxt = "n", pch = 21, bg = 3, xlab = "Number of Respondents", 
+          ylab = "Proportion of (B)", main = "Bilingual Education Survey")
   
   axis(2, at = seq(0, 1L, len = 6), labels = paste0(seq(0, 1e2, len = 6), "%"))
   
-  abline(h = pBi, lty = 2, col = 2)
-  
-  text(mean(par("usr")[1:2]), pBi, "TRUE Proportion of \"B\"", pos = 3, font = 2, col = 4)
-  
-  ResponseLetters = paste( c("M","B")[ResponseSequence[1L:1e1] + 1L], collapse ="" )    # Display the first 10 response sequence
+  ResponseLetters = paste( c("M","B")[ResponseSequence[1L:1e1] + 1L], collapse = "")  # Display the first 10 response sequence
   
   displayString = paste0("Response Sequence = ", ResponseLetters, ". . .")
   
